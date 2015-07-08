2efd7d081eb1c1030844e5a7a86b3a3f64e578d6
albert925 <albertarias925@gmail.com>
2015-07-08 18:05:23 -0500

    abogados

---
+++ index.php
@@ -0,0 +1,271 @@
+<?php
+	include 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, maximun-scale=1" />
+	<meta name="description" content="somos una empresa en el sector inmobiliario de la ciudad de Cúcuta y su área metropolitana. 
+	El servicio al cliente interno será una responsabilidad incorporada en todos los miembros de la organización." />
+	<meta name="keywords" content="Abogados, noticias, denuncias, casos" />
+	<title>Vargas Nossa y Asosciados</title>
+	<link rel="icon" href="imagenes/icon.png" />
+	<link rel="image_src" href="imagenes/logo.png" />
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/iconos/style.css" />
+	<link rel="stylesheet" href="css/default/default.css" />
+	<link rel="stylesheet" href="css/nivo_slider.css" />
+	<link rel="stylesheet" href="css/owl_carousel.css" />
+	<link rel="stylesheet" href="css/owl_theme_min.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<script src="js/jquery_2_1_1.js"></script>
+	<script src="js/owl_carousel_min.js"></script>
+	<script src="js/scripag.js"></script>
+	<script type="application/ld+json">
+		{
+		  "@context" : "http://schema.org",
+		  "@type" : "LocalBusiness",
+		  "name" : "Vargas Nossa y Asosciados",
+		  "image" : "url"
+		}
+	</script>
+</head>
+<body>
+	<header id="automargen">
+		<article id="verlogo">
+			<figure id="logo">
+				<a href="">
+					<img src="imagenes/logo.png" alt="logo" />
+				</a>
+			</figure>
+		</article>
+		<article id="minmyred">
+			<article id="spanmm">
+				<a href="registro">usuario</a>
+				<span>contacto@vargasnossa.com</span>
+				<span>(+57)5 72 0821</span>
+			</article>
+			<article id="redes">
+				<a href="" target="_blank"><span class="icon-facebook3"></span></a>
+				<a href="" target="_blank"><span class="icon-twitter3"></span></a>
+				<a href="" target="_blank"><span class="icon-instagram-with-circle"></span></a>
+			</article>
+		</article>
+	</header>
+	<article id="automargen" class="menuP">
+		<nav id="mnP">
+			<a class="sel" href="">Inicio</a>
+			<a href="portafolio">Portafolio de Servicios</a>
+			<a href="nosotros">Quienes Somos</a>
+			<a href="contacto">Contacto</a>
+		</nav>
+		<div id="mn_mv"><span class="icon-menu"></span></div>
+	</article>
+	<section>
+		<article id="automargen" class="innots">
+			<article id="textowl">
+				<div class="slider-wrapper theme-default">
+					<div id="slider" class="nivoSlider">
+						<img src="imagenes/galery/fondo.jpg" alt="imagen" title="#caption1" />
+					</div>
+					<div div id="caption1" style="display: none;">
+						<h2>Tienes una idea para tu empresa</h2>
+					</div>
+				</div>
+			</article>
+			<nav>
+				<a href="#zero">Casos Recientes</a>
+				<a href="#dos">En proceso</a>
+				<a href="">Articulo de Interés</a>
+			</nav>
+			<article class="noticiasR">
+				<article class="owl-carousel owl-theme owl-loaded">
+					<div class="item">
+						<figure class="notitem" data-hash="zero">
+							<img src="imagenes/noticias/VN_web.jpg" alt="imagen" />
+							<figcaption>
+								<h4>Titulo</h4>
+								<div>
+									Pequeña descripción
+								</div>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="item">
+						<figure class="notitem">
+							<img src="imagenes/noticias/VN_web.jpg" alt="imagen" />
+							<figcaption>
+								<h4>Titulo</h4>
+								<div>
+									Pequeña descripción
+								</div>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="item">
+						<figure class="notitem">
+							<img src="imagenes/noticias/VN_web.jpg" alt="imagen" />
+							<figcaption>
+								<h4>Titulo</h4>
+								<div>
+									Pequeña descripción
+								</div>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="item">
+						<figure class="notitem" data-hash="dos">
+							<img src="imagenes/noticias/VN_web.jpg" alt="imagen" />
+							<figcaption>
+								<h4>Titulo</h4>
+								<div>
+									Pequeña descripción
+								</div>
+							</figcaption>
+						</figure>
+					</div>
+				</article>
+			</article>
+		</article>
+	</section>
+	<section class="dossec">
+		<article id="automargen" class="flab">
+			<article>
+				<figure style="background-image:url(imagenes/fondo.jpg);"></figure>
+				<figcaption>
+					<h3>Nombre</h3>
+					<article>
+						Texto
+					</article>
+				</figcaption>
+			</article>
+		</article>
+	</section>
+	<section class="frases">
+		<article id="automargen">
+			<article class="owl-carousel-b owl-theme owl-loaded">
+				<div class="item">
+					<article class="fraitem">
+						<h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</h2>
+						<p>
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+							commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+							dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+							sunt in culpa qui officia deserunt mollit anim id est laborum."
+						</p>
+					</article>
+				</div>
+				<div class="item">
+					<article class="fraitem">
+						<h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</h2>
+						<p>
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+							commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+							dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+							sunt in culpa qui officia deserunt mollit anim id est laborum."
+						</p>
+					</article>
+				</div>
+			</article>
+		</article>
+	</section>
+	<section class="tressec flxcont">
+		<article>
+			<form action="#" method="post" class="columninput">
+				<input type="text" id="nmct" required placeholder="Nombre" />
+				<input type="email" id="corct" required placeholder="E-mail" />
+				<textarea rows="4" id="txct" required placeholder="Mensaje"></textarea>
+				<div id="txsj"></div>
+				<input type="submit" value="Enviar" id="nvcas" />
+			</form>
+		</article>
+		<article>
+			<h2>Cuéntanos tu caso</h2>
+		</article>
+	</section>
+	<footer>
+		<article class="flexfoot">
+			<article id="contfo">
+				<h4>VARGAS NOSSA & ASOCIADOS</h4>
+				<div><span class="icon-location"></span>Av. 1E # 11-142 Barrio Caobos</div>
+				<div>Colombia - Cúcuta</div>
+				<div>
+					<span class="icon-phone"></span>
+					314 394 1701 - 315 827 4399
+				</div>
+				<div>
+					<span class="icon-old-phone"></span>
+					583 0897/98 - 573 0190 - 571 22 62
+				</div>
+				<div><span class="icon-mail"></span> contacto@vargasnossaabogados.com</div>
+			</article>
+			<article id="mapagoogle">
+				<article id="map_canvas" class="mapas"></article>
+			</article>
+		</article>
+	</footer>
+	<script src="js/nivo_slider.js"></script>
+	<script type="text/javascript">
+		$(window).load(function(){
+      $('#slider').nivoSlider({
+          effect: 'fade',
+          slices: 15,
+          boxCols: 8,
+          boxRows: 4,
+          animSpeed: 500,
+          pauseTime: 10000,
+          pauseOnHover:true,
+          startSlide: 0,
+          directionNav: true,
+          controlNav: true,
+          controlNavThumbs: false,
+          pauseOnHover: true,
+          manualAdvance: false,
+          prevText: 'Prev',
+          nextText: 'Next',
+          randomStart: false,
+      });
+   	});
+   	// http://web.tursos.com/como-implementar-nivo-slider-en-tu-pagina-web/
+	</script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+		  $('.owl-carousel').owlCarousel({
+				autoplay:false,
+				loop:false,
+				margin:10,
+				responsiveClass:true,
+				URLhashListener:true,
+				startPosition: 'URLHash',
+				nav:false,
+				responsive:{
+					0:{
+						items:1
+					},
+					600:{
+						items:2
+					},
+					1000:{
+						items:3
+					}
+				}
+			});
+			$(".owl-carousel-b").owlCarousel({
+				autoHeight: false,
+    		autoHeightClass: 'owl-height',
+    		autoplay:true,
+				autoplayTimeout:3000,
+				autoplayHoverPause:true,
+    		items:1
+			});
+		});
+	</script>
+	<script src="http://www.google.com/jsapi"></script>
+	<script src="js/colmapa.js"></script>
+</body>
+</html>
---
+++ js/scripag.js
@@ -0,0 +1,27 @@
+var contador=1;
+$(document).on("ready",inicio_pagina);
+function inicio_pagina () {
+	if ($(window).width()<810) {
+		$(window).scroll(mostrarmov);
+	}
+	$("#mn_mv").on("click",abrir_menu);
+}
+function abrir_menu () {
+	if (contador==1) {
+		$("#mnP").animate({left:"0"}, 500);
+		contador=0;
+	}
+	else{
+		$("#mnP").animate({left:"-100%"}, 500);
+		contador=1;
+	}
+}
+function mostrarmov () {
+	var altoscroll=$(window).scrollTop();
+	if (altoscroll>250) {
+		$(".menuP").css({position:"fixed"});
+	}
+	else{
+		$(".menuP").css({position:"relative"});
+	}
+}
