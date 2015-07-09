d576fff96aa5b1f7c5be0f34fcdc322115b949bc
albert925 <albertarias925@gmail.com>
2015-07-09 17:54:40 -0500

    mf3

--- js/scripadmins.js
+++ js/scripadmins.js
@@ -1,6 +1,7 @@
 $(document).on("ready",inicscripadmin);
 function inicscripadmin () {
 	$("#btA").on("click",abrirA);
+	$("#btB").on("click",abrirB);
 	$(".dell").on("click",confirborr);
 }
 function confirborr () {
@@ -10,12 +11,31 @@ function abrirA (aA) {
 	aA.preventDefault();
 	$("#cjA").each(animarA);
 }
+function abrirB (bB) {
+	bB.preventDefault();
+	$("#cjB").each(animarB);
+}
 function animarA () {
 	if ($(window).width()>800) {
 		var alto="250px";
 	}
 	else{
 		var alto="350px";
+	}
+	varcajalt=$(this).css("height");
+	if (varcajalt==alto) {
+		$(this).animate({height:"0"}, 500);
+	}
+	else{
+		$(this).animate({height:alto}, 500);
+	}
+}
+function animarB () {
+	if ($(window).width()>800) {
+		var alto="750px";
+	}
+	else{
+		var alto="850px";
 	}
 	varcajalt=$(this).css("height");
 	if (varcajalt==alto) {
